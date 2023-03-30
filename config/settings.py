9e29d2ffe8f10ec043cd18165aa6768e35d9707a
nodo1014 <htmsejong@gmail.com>
2023-03-31 05:19:57 +0900

    이미지, 파일 업로드 기능, picsum.photos post.head_image.url

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,3 +87,6 @@ USE_TZ = False
 STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path, include
+
 
 urlpatterns = [
     path('blog/', include('blog.urls')),
     path('', include('single_pages.urls')),
     path('admin/', admin.site.urls),
 ]
+
+# static() 정적 파일들의 url을 관리. 접근한 url을 리턴.
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
