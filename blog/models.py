608741b5cccdf4a52054f12bbab3c63800211332
nodo1014 <htmsejong@gmail.com>
2023-03-31 08:29:29 +0900

    10장 템플릿 필터.hook_text 첨부파일 아이콘

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,14 @@
+import os
 from django.db import models
 #django 라이브러리, db.models 모듈 임포트 후, Model클래스 상속
 #Post/title,content,created_at
 class Post(models.Model):
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=80)
+    hook_text = models.CharField(max_length=120, blank=True)
     content = models.TextField()
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
-    file_upload = models.ImageField(upload_to='blog/files/%Y/%m/%d/', blank=True)
+    file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -16,4 +18,13 @@ class Post(models.Model):
     
     def get_absolute_url(self):
         return f'/blog/{self.pk}'
+    
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+    # basename(): 파일이름만
+# split(파일) : 디렉토리와 파일 분리, splitext():확장자만, dirname: 디렉토리만 ,   isdir(): 디렉토리인지?
+    def get_file_ext(self):
+
+        return self.get_file_name().split('.')[-1]
+        # return os.path.splitext(self.file_upload.name)
 
